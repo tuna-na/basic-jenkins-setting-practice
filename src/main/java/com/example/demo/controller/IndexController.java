3a7a10454b94cb5ebe570001a2214c27b542df3c
kmw <mw51321681@gmail.com>
2021-12-21 15:40:05 +0900

    add IndexController

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
+
+    @GetMapping("/")
     public String hello() {
 
         return "hello, jenkins";
